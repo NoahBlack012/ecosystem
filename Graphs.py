96c932354f9029a928a1d08cff37a73933f52fe8
N B <eastbo2004@gmail.com>
2020-10-26 21:58:01 -0400

    Initial set up for graphing of simulation

--- Graphs.py
+++ Graphs.py
@@ -1,11 +1,27 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import animation
 
+from Sim import sim
+
+"""
+EX
+{'cycle': 67, 'pop': 0, 'animals': {'speed': [], 'range': []}}
+"""
 class graphs:
     """Class to manage graphing the simulation"""
     def __init__(self):
-        pass
-    def plot(self, population):
-        pass
+        self.fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        self.sim = sim()
+        sim.run()
+        self.data = sim.datasets
+
+    def plot(self):
+        def init_func():
+            return
+        anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
 
 if __name__ == '__main__':
     g = graphs()
